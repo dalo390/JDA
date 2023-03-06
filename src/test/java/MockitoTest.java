272eb207a8092ad85eeb35a7e381fb5422df7bed
Kevin-the-man <56621344+Kevin-the-man@users.noreply.github.com>
2023-03-06 13:59:08 -0800

    update mockito

--- src/test/java/MockitoTest.java
+++ src/test/java/MockitoTest.java
@@ -1,57 +1,57 @@
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.audit.ThreadLocalReason;
-import net.dv8tion.jda.api.events.ExceptionEvent;
-import net.dv8tion.jda.api.events.http.HttpRequestEvent;
-import net.dv8tion.jda.api.exceptions.ContextException;
-import net.dv8tion.jda.api.exceptions.ErrorResponseException;
-import net.dv8tion.jda.api.exceptions.RateLimitedException;
-import net.dv8tion.jda.api.requests.Request;
-import net.dv8tion.jda.internal.JDAImpl;
-import net.dv8tion.jda.internal.requests.CallbackContext;
-import net.dv8tion.jda.internal.requests.RestActionImpl;
-import net.dv8tion.jda.internal.requests.Route;
-import net.dv8tion.jda.internal.utils.IOUtil;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import org.apache.commons.collections4.map.CaseInsensitiveMap;
+// import net.dv8tion.jda.api.audit.ThreadLocalReason;
+// import net.dv8tion.jda.api.events.ExceptionEvent;
+// import net.dv8tion.jda.api.events.http.HttpRequestEvent;
+// import net.dv8tion.jda.api.exceptions.ContextException;
+// import net.dv8tion.jda.api.exceptions.ErrorResponseException;
+// import net.dv8tion.jda.api.exceptions.RateLimitedException;
+// import net.dv8tion.jda.api.requests.Request;
+// import net.dv8tion.jda.internal.JDAImpl;
+// import net.dv8tion.jda.internal.requests.CallbackContext;
+// import net.dv8tion.jda.internal.requests.RestActionImpl;
+// import net.dv8tion.jda.internal.requests.Route;
+// import net.dv8tion.jda.internal.utils.IOUtil;
+// import okhttp3.MultipartBody;
+// import okhttp3.RequestBody;
+// import org.apache.commons.collections4.map.CaseInsensitiveMap;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import javax.print.attribute.standard.ColorSupported;
-import javax.xml.ws.Response;
+// import javax.annotation.Nonnull;
+// import javax.annotation.Nullable;
+// import javax.print.attribute.standard.ColorSupported;
+// import javax.xml.ws.Response;
 
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.TimeoutException;
-import java.util.function.BooleanSupplier;
-import java.util.function.Consumer;
+// import java.util.concurrent.CancellationException;
+// import java.util.concurrent.TimeoutException;
+// import java.util.function.BooleanSupplier;
+// import java.util.function.Consumer;
 
 
 import org.mockito.Mock;
-import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+// import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.initMocks;
+//import static org.mockito.MockitoAnnotations.initMocks;
 
 
-import net.dv8tion.jda.api.entities.EmbedType;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.internal.entities.EntityBuilder;
-import net.dv8tion.jda.internal.utils.Checks;
-import net.dv8tion.jda.internal.utils.Helpers;
+// import net.dv8tion.jda.api.entities.EmbedType;
+// import net.dv8tion.jda.api.entities.MessageEmbed;
+// import net.dv8tion.jda.api.entities.Role;
+// import net.dv8tion.jda.internal.entities.EntityBuilder;
+// import net.dv8tion.jda.internal.utils.Checks;
+// import net.dv8tion.jda.internal.utils.Helpers;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+// import javax.annotation.Nonnull;
+// import javax.annotation.Nullable;
 import java.awt.*;
-import java.time.OffsetDateTime;
-import java.time.temporal.TemporalAccessor;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.regex.Pattern;
+// import java.time.OffsetDateTime;
+// import java.time.temporal.TemporalAccessor;
+// import java.util.LinkedList;
+// import java.util.List;
+// import java.util.regex.Pattern;
 
 
 
